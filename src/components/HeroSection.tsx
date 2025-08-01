f35e49766abf1262f17ab62a62aba177019a90fe
rahul.ragtah <rahul.ragtah@power2sme.com>
2025-08-01 20:33:11 +0530

    scroll fixed

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -130,7 +130,7 @@ const HeroSection = () => {
       </div>
       
       {/* Scroll indicator */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+      <div className="absolute bottom-0 lg:bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
         <div className="w-6 h-10 border-2 border-gray-600 rounded-full flex justify-center">
           <div className="w-1 h-3 bg-gray-400 rounded-full mt-2" />
         </div>
